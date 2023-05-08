40af93accd3f6cd2b8e0c359222b9729328fbce6
Sam <kkaammaahh516@gmail.com>
2023-05-09 00:05:21 +0300

    changes

--- src/Exhibits.js
+++ src/Exhibits.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import exhibitionImage from "./assets/exhibition.jpg";
+// import exhibitionImage from "./assets/exhibition.jpg";
 
 const Exhibits = () => {
   const [countdown, setCountdown] = useState(null);

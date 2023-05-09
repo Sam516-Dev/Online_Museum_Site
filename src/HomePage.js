052b3b1c5a857ee51ff36e0de91b8555613e6e11
Sam <kkaammaahh516@gmail.com>
2023-05-09 21:36:18 +0300

    upadates

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,7 +9,7 @@ function HomePage() {
         <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h1 className="mt-2 text-3xl font-extrabold text-gray-900 sm:text-4xl">
-              The Online Museum
+            Food Appetites
             </h1>
             <p className="mt-1 text-xl text-gray-600">
               Explore our vast collections and exhibits.
